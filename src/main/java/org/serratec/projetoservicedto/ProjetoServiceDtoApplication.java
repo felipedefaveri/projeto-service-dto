0f5c39f42bd97cce4c28da41b9c5591650cfcd6c
felipedefaveri <felipefaver27@gmail.com>
2021-11-05 19:45:32 -0300

    alteração projeto

--- src/main/java/org/serratec/projetoservicedto/ProjetoServiceDtoApplication.java
+++ src/main/java/org/serratec/projetoservicedto/ProjetoServiceDtoApplication.java
@@ -1,4 +1,4 @@
-git config package org.serratec.projetoservicedto;
+package org.serratec.projetoservicedto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
